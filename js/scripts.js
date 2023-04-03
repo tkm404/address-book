d66662e6154f5904f90ff3eb932e42057a76f017
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-03 16:45:50 -0700

    add address book example.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// BL for AddressBook -----
+function AddressBook() {
+    this.contacts = {};
+    this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+    return false;
+};
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+  return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+//BL for Contacts -----
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
