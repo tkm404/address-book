c031c6682b53974e9b70a6a125f769e3ce7c9d5b
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-04 13:18:27 -0700

    add mutlitple email entires

--- js/scripts.js
+++ js/scripts.js
@@ -29,15 +29,28 @@ AddressBook.prototype.deleteContact = function(id) {
 };
 
 //BL for Contacts -----
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, addresses, homeAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.addresses = addresses;
+  this.homeAddress = homeAddress;
+}
+
+Contact.prototype.addAddress = function(emailAddress) {
+  this.emailAddress[emailAddress.workAdd] = emailAddress
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+//BL for Addresses
+function Address(workAdd, homeAdd) {
+this.workAdd = workAdd;
+this.homeAdd = homeAdd;
+}
+
 
 // UI Logic vvv ------
 
@@ -62,6 +75,8 @@ const contact = addressBook.findContact(event.target.id);
 document.querySelector(".first-name").innerText = contact.firstName;
 document.querySelector(".last-name").innerText = contact.lastName;
 document.querySelector(".phone-number").innerText = contact.phoneNumber;
+document.querySelector(".email-address").innerText = (contact.addresses.workAdd + " " + contact.addresses.homeAdd)
+document.querySelector(".home-address").innerText = contact.homeAddress;
 document.querySelector("div#contact-details").removeAttribute("class");
 document.querySelector("button.delete").setAttribute("id", contact.id);
 document.querySelector("div#contact-details").removeAttribute("class");
@@ -72,12 +87,19 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  const inputtedHome = document.querySelector("input#new-home-address").value;
+  const inputtedWorkEmail = document.querySelector("input#new-work-email").value;
+  const inputtedHomeEmail = document.querySelector("input#new-home-email").value;
+  const inputtedEmail = new Address(inputtedWorkEmail, inputtedHomeEmail);
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail,inputtedHome);
   addressBook.addContact(newContact);
   listContacts(addressBook);
   document.querySelector("input#new-first-name").value = null;
   document.querySelector("input#new-last-name").value = null;
   document.querySelector("input#new-phone-number").value = null;
+  document.querySelector("input#new-work-email").value = null;
+  document.querySelector("input#new-home-email").value = null;
+  document.querySelector("input#new-home-address").value = null;
 }
 
 function handleDelete(event) {
